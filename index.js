a809f9c05828a08d86d52a833470ecd39fe8aae1
Joe Finny <finne132@umn.edu>
2018-02-24 12:53:48 -0600

    saving this exercise, creating objects that hold keypairs

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 "use strict";
 
+const joe = {
+    name:'Joe',
+    age:28,
+    happy:true,
+    hairColor:"brown",
+}
+
+const sean  ={
+    name:'Sean',
+    age:21,
+    happy:true,
+    hairColor:'brown',
+}
+
+console.log(typeof joe, joe);
+console.log(typeof sean, sean);
